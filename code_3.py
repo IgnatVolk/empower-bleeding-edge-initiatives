84280a6685a2f0941831de9f0e1e1e3de08ee71e
IgnatVolk <142455386+IgnatVolk@users.noreply.github.com>
2023-12-28 17:08:07 +0200

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,25 @@
+        }
+        for (int i = 0; i < 10; i++) {
+            data[i] = (int) (Math.random() * 100) + 1;
+            System.out.println("Random Number: " + item);
+public class RandomDataGenerator {
+
+        for (int item : data) {
+        }
+}
+        int[] data = new int[10];
+    public static void main(String[] args) {
+    }
+        int[] data = new int[10];
+        }
+        for (int item : data) {
+            data[i] = (int) (Math.random() * 100) + 1;
+}
+
+            System.out.println("Random Number: " + item);
+    public static void main(String[] args) {
+public class RandomDataGenerator {
+        for (int i = 0; i < 10; i++) {
+
+    }
+        }
